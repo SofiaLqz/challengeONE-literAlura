201e03ec59d04bf59c1da5c47149b20eab4df8fa
Sofia <sofialqz@gmail.com>
2024-07-13 19:06:46 -0300

    Crea y configura el proyecto en Spring Initializr

---
+++ src/main/java/com/aluracursos/challengeone_literalura/ChallengeoneLiteraluraApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.challengeone_literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeoneLiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeoneLiteraluraApplication.class, args);
+	}
+
+}
