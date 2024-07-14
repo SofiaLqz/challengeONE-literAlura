ca2a155a8a3f03ff8ff4cb9bc7bbc94bbbc4c85b
Sofia <sofialqz@gmail.com>
2024-07-14 12:55:30 -0300

    Realiza primera solicitud de prueba a la API Gutendex

--- src/main/java/com/aluracursos/challengeone_literalura/ChallengeoneLiteraluraApplication.java
+++ src/main/java/com/aluracursos/challengeone_literalura/ChallengeoneLiteraluraApplication.java
@@ -1,13 +1,20 @@
 package com.aluracursos.challengeone_literalura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChallengeoneLiteraluraApplication {
+public class ChallengeoneLiteraluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeoneLiteraluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		var usarApi = new UsarApi();
+		String json = usarApi.obtenerDatos("https://gutendex.com/books/");
+		System.out.println(json);
+	}
 }
